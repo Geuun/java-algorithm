d3181c848c2a4e70093fc30a41a733afbef9d212
KimJaeGeun <jgk9612@gmail.com>
2022-10-31 09:31:12 +0900

    feat: Add my logic

---
+++ src/main/java/algorithm/programmers/PracticeTest.java
@@ -0,0 +1,14 @@
+package algorithm.programmers;
+
+/**
+ * 1. 메소드 설계 -> int[]를 받아서 int[]를 리턴하는 method
+ * 2. 각 수포자가 찍는 번호의 반복을 배열에 저장
+ * 3. 가장 많이 맞춘 사람을 저장할 result 배열 선언
+ * 4. 반복문을 통해서 각 수포자의 정답이 맞는지 체크하고 맞다면 각 정답 배열 인덱스에 +1
+ */
+public class PracticeTest {
+    public int[] solution(int[] answers) {
+        int[] supo1 = {1, 2, 3, 4, 5};
+        int[]
+    }
+}
