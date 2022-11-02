7c7a55353fabe95beeb72bdba900d7fd0411419f
KimJaeGeun <jgk9612@gmail.com>
2022-11-02 09:26:56 +0900

    style: Modify code

--- src/main/java/algorithm/programmers/primenumber/PrimeNumber.java
+++ src/main/java/algorithm/programmers/primenumber/PrimeNumber.java
@@ -38,28 +38,20 @@ public class PrimeNumber {
 
     public int solution(int n) {
         int answer = 0;
-
         int[] arr = new int[n + 1];
+
         for (int i = 2; i <= n; i++) {
-            arr[i] = i; // 2부터 n 까지의 수를 배열에 담기
-        }
-
+            arr[i] = i;
+        }// 2부터 n 까지의 수를 배열에 담기
         for (int i = 2; i * i < n; i++) {
-//            System.out.println("i: "+ i);
-            if (arr[i] == 0) continue;
-            for (int j = 2 * i; j <= n; j += i) {
-//                System.out.println("j: " + j);
-                arr[j] = 0;
-//                System.out.println("arr: " + Arrays.toString(arr));
-            }
-        }
-
-//        System.out.println("arr: " + Arrays.toString(arr));
+            if (arr[i] == 0) continue; // 이미 0으로 바뀐 인덱스는 넘어간다.
+            for (int j= i * i ; j<n+1; j+=i) arr[j] = 0;
+        }// 소수의 배수들을 배열에서 0으로 변경 but 이미 0이라면 continue한다.
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) answer++;
-        }
-//        System.out.println("result: " +answer);
+        }// 반복문을 배열의 길이만큼 돌면서 0이 아닌 수를 answer에 카운트한다.
+
         return answer;
     }
 
