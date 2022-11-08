22e75a774509b0df8488be82a688c976c6b59a31
KimJaeGeun <jgk9612@gmail.com>
2022-11-08 10:31:38 +0900

    feat: Add BinarySearch algorithm step1
    이진탐색 구현해보기

---
+++ src/main/java/algorithm/datastructure/BinarySearch/BinarySearch.java
@@ -0,0 +1,20 @@
+package algorithm.datastructure.BinarySearch;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int targetNum = 7;
+
+        int startIdx = 0;
+        int endIdx = nums.length;
+        int middleIdx = endIdx - startIdx / 2;
+
+        if (nums[middleIdx] > targetNum) {
+            startIdx = middleIdx + 1;
+        } else if (nums[middleIdx] < targetNum) {
+            endIdx = middleIdx - 1;
+        }
+
+    }
+}
