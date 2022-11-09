900ae78612f84a02fdb48d67264c16290df558f1
KimJaeGeun <jgk9612@gmail.com>
2022-11-09 09:44:02 +0900

    feat: Add SelectionSort algorithm  Decending ver

--- src/main/java/algorithm/datastructure/selectionsort/SelectionSort.java
+++ src/main/java/algorithm/datastructure/selectionsort/SelectionSort.java
@@ -20,9 +20,27 @@ public class SelectionSort {
         return nums;
     }
 
+    public int[] SelectionSortDesc(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIdx = i;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[minIdx];
+            nums[minIdx] = temp;
+        }
+        return nums;
+    }
+
     public static void main(String[] args) {
         SelectionSort selectionSort = new SelectionSort();
         int[] nums = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
         System.out.println(Arrays.toString(selectionSort.SelectionSort(nums)));
+        System.out.println(Arrays.toString(selectionSort.SelectionSortDesc(nums)));
     }
 }
