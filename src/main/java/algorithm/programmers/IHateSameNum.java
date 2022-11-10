9e7b00aeaa5a187fb017a1481371865fa7f1f02b
KimJaeGeun <jgk9612@gmail.com>
2022-11-10 10:12:56 +0900

    feat: Add programers IHateSameNum algorithm solution Using Stack

--- src/main/java/algorithm/programmers/IHateSameNum.java
+++ src/main/java/algorithm/programmers/IHateSameNum.java
@@ -3,6 +3,7 @@ package algorithm.programmers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 같은 숫자는 싫어
@@ -24,7 +25,22 @@ public class IHateSameNum {
         for (int i = 0; i < nums.size(); i++) {
             answer[i] = nums.get(i);
         }
-        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+
+    public int[] stackSolution(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+
+        st.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (st.peek() != arr[i]) {
+                st.push(arr[i]);
+            }
+        }
+        int[] answer = new int[st.size()];
+        for (int i = 0; i < st.size(); i++) {
+            answer[i] = st.get(i);
+        }
         return answer;
     }
 
@@ -34,5 +50,7 @@ public class IHateSameNum {
         int[] input2 = {4, 4, 4, 3, 3}; // 4, 3
         iHateSameNum.solution(input1);
         iHateSameNum.solution(input2);
+        System.out.println(Arrays.toString(iHateSameNum.stackSolution(input1)));
+
     }
 }
