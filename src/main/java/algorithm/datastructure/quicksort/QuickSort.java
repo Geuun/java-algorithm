26ed99aa4da5ac87cda2ea2054dfbc252025e674
KimJaeGeun <jgk9612@gmail.com>
2022-11-15 10:39:17 +0900

    feat: Add QuickSort Algorithm Step3

--- src/main/java/algorithm/datastructure/quicksort/QuickSort.java
+++ src/main/java/algorithm/datastructure/quicksort/QuickSort.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuickSort {
+
+    public List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right) {
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+        return answer;
+    }
     public List<Integer> sort(List<Integer> arr) {
         /**
          * 1. 기준 값 뽑는 로직 구현
@@ -12,21 +20,30 @@ public class QuickSort {
 
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
 
-        int pivot = arr[arr.length / 2]; // 5
+        int pivot = arr.get(arr.size() / 2); // 5
 
-        for (int i = 0; i < arr.length  ; i++) {
-            if (pivot > arr[i]) left.add(arr[i]);
-            else if (pivot < arr[i]) right.add(arr[i]);
+        for (int i = 0; i < arr.size()  ; i++) {
+            if (pivot > arr.get(i)) left.add(arr.get(i));
+            else if (pivot < arr.get(i)) right.add(arr.get(i));
         }
 
         System.out.println(left);
-        System.out.println(right); // 20, 18, 19, 25, 40 ,50
+        System.out.println(right);
+        System.out.println(mid);
 
-        return sort(left) + pivot + sort(right); // <-- 재귀 함수로 구현필요
+        return merge(sort(left), sort(mid), sort(right));
     }
 
     public static void main(String[] args) {
-        var arr = new int[] {20, 18, 5, 19, 5, 25, 40, 50}; // size = 8
+        QuickSort qs = new QuickSort();
+        var arr = new int[] {20, 18, 5, 19, 5, 25, 40, 50};
+
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            al.add(arr[i]);
+        }
+        qs.sort(al);
     }
 }
