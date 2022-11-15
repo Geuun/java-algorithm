5bcedb99b104d5d1deb2d86ef20c0e09a52d3ca1
KimJaeGeun <jgk9612@gmail.com>
2022-11-15 09:50:05 +0900

    feat: Add QuickSort Algorithm Step1

---
+++ src/main/java/algorithm/datastructure/quicksort/QuickSort.java
@@ -0,0 +1,29 @@
+package algorithm.datastructure.quicksort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        var arr = new int[] {20, 18, 5, 19, 5, 25, 40, 50}; // size = 8
+
+        /**
+         * 1. 기준 값 뽑는 로직 구현
+         * 2. 기준 값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
+         */
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        int pivot = arr[arr.length / 2]; // 5
+
+        for (int i = 0; i < arr.length  ; i++) {
+            if (pivot > arr[i]) left.add(arr[i]);
+            else if (pivot < arr[i]) right.add(arr[i]);
+        }
+
+        System.out.println(left);
+        System.out.println(right); // 20, 18, 19, 25, 40 ,50
+    }
+}
