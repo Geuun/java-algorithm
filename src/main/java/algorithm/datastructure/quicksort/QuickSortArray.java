3198fe51e2ff9192528c75c8038a22adfb8962c9
KimJaeGeun <jgk9612@gmail.com>
2022-11-16 09:43:26 +0900

    feat: Add QuickSort Algorithm Array ver step1

--- src/main/java/algorithm/datastructure/quicksort/QuickSortArray.java
+++ src/main/java/algorithm/datastructure/quicksort/QuickSortArray.java
@@ -8,5 +8,23 @@ package algorithm.datastructure.quicksort;
  * 5. 교환 했으면 leftIdx += 1, rightIdx -= 1
  */
 public class QuickSortArray {
-    
+    public static void main(String[] args) {
+        var arr = new int[]{20, 18, 5, 19, 40, 50, 5,25};
+        int pivot = arr[arr.length / 2]; // pivot = 40
+        System.out.println("pivot: " + pivot);
+
+        int leftIdx = 0;
+        int rightIdx = arr.length;
+        int temp;
+
+        for (leftIdx = 0; leftIdx < arr.length / 2; leftIdx++) {
+            if (arr[leftIdx] < pivot) leftIdx += 1;
+        }
+        for (rightIdx = arr.length; rightIdx > arr.length / 2; rightIdx++) {
+            if (arr[rightIdx] > pivot) rightIdx -= 1;
+        }
+//        if (arr[leftIdx] <= rightIdx) {
+//
+//        }
+    }
 }
