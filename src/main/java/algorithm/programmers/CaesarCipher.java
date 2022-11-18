7bfa4e8fc3479c4ee51ca657a7e7c0d67a82e8aa
KimJaeGeun <jgk9612@gmail.com>
2022-11-18 10:02:14 +0900

    fix: CaesarCipher Algorithm complete
    해결완료

--- src/main/java/algorithm/programmers/CaesarCipher.java
+++ src/main/java/algorithm/programmers/CaesarCipher.java
@@ -13,21 +13,30 @@ public class CaesarCipher {
         char[] chs = s.toCharArray();
         // 아스키코드
         for (char ch : chs) {
-            if (ch != 32) { //아스키코드 32번 공백
-                if(ch == 90 || ch == 122) { // 90:Z -> 65:A 122:z -> 97:a
-                    ch -= 26;
+            if (ch == 32) { //아스키코드 32번 공백
+                answer += " "; // 32번이면 공백추가
+            } else {
+                if (ch <= 90) {
+                    ch += n;
+                    if (ch > 90) ch -= 26; //90:Z -> 65:A
+                } else {
+                    ch += n;
+                    if (ch > 122) ch -= 26; //122:z -> 97:a
                 }
-                ch += n;
                 answer += ch;
-            } else answer += " ";
+            }
         }
-
-
         return answer;
     }
 
     public static void main(String[] args) {
+        CaesarCipher cp = new CaesarCipher();
+        String s1 = "AB";
+        String s2 = "z";
+        String s3 = "a B z";
 
+        System.out.println(cp.solution(s1, 1));
+        System.out.println(cp.solution(s2, 1));
+        System.out.println(cp.solution(s3, 4));
     }
-
 }
