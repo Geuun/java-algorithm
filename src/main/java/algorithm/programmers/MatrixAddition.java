0be71023f6fcddc41f9349b8516a558aa2c4f91d
KimJaeGeun <jgk9612@gmail.com>
2022-11-18 10:22:35 +0900

    feat: Add MatrixAddition Algorithm

---
+++ src/main/java/algorithm/programmers/MatrixAddition.java
@@ -0,0 +1,20 @@
+package algorithm.programmers;
+
+/**
+ * 행렬의 덧셈
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12950
+ */
+public class MatrixAddition {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        /**
+         * 조건: 행열은 같다.
+         */
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1.length ; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
