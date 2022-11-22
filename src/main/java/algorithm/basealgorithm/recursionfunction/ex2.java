369447915c5661828d479ab4b9f1d4061315e389
KimJaeGeun <jgk9612@gmail.com>
2022-11-22 10:50:01 +0900

    feat: Add sumArray recursionfunction exercise

---
+++ src/main/java/algorithm/basealgorithm/recursionfunction/ex2.java
@@ -0,0 +1,15 @@
+package algorithm.basealgorithm.recursionfunction;
+
+/**
+ * ArraySum 재귀
+ */
+public class ex2 {
+    static int sum(int[] arr) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 3, 2, 9};
+        int r = sum(arr);
+        System.out.println(r);
+    }
+}
---
+++ src/main/java/algorithm/codeup/C1851.java
@@ -0,0 +1,10 @@
+package algorithm.codeup;
+
+public class C1851 {
+    public void printstar(int num) {
+        if (num <= 0) {
+            return;
+        }
+
+    }
+}
