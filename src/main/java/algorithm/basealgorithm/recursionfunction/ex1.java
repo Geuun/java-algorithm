75fb6ac3ea3c6aff1596b99a82c1e99b8848439e
KimJaeGeun <jgk9612@gmail.com>
2022-11-22 09:26:54 +0900

    feat: Add print1To100 for using recursionfunction

--- src/main/java/algorithm/basealgorithm/recursionfunction/ex1.java
+++ src/main/java/algorithm/basealgorithm/recursionfunction/ex1.java
@@ -1,9 +1,13 @@
 package algorithm.basealgorithm.recursionfunction;
 
 public class ex1 {
-    public void print1To100 (int n) {
-        System.out.println(n);
-        print1To100(n);
+    public void print1To100 (int num) {
+        if (num > 100) {
+            return;
+        }
+
+        System.out.println(num);
+        print1To100(num + 1);
     }
 
     public static void main(String[] args) {
