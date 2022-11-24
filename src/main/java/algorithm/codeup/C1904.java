c6f181c2dc1363bb93e1561780993a6a63535f18
KimJaeGeun <jgk9612@gmail.com>
2022-11-24 10:16:08 +0900

    feat: Add Codeup C1904 printOddNum with recurcivefunction

--- src/main/java/algorithm/codeup/C1904.java
+++ src/main/java/algorithm/codeup/C1904.java
@@ -1,13 +1,27 @@
 package algorithm.codeup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class C1904 {
 
     static void pritnOddNum(int x, int y) {
 
-        // 나눗셈 연산
-        if (x % 2 == 0) x++;
-        if (y % 2 == 0) y++;
+        // 나눗셈 연산 2, 7
+        if (x > y) { return; }
+        if (x % 2 != 0) {
+            System.out.printf("%d ", x);
+        }
+        pritnOddNum(x + 1, y);
+    }
 
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = bf.readLine().split(" ");
+        int x = Integer.parseInt(input[0]);
+        int y = Integer.parseInt(input[1]);
+
+        pritnOddNum(x, y);
     }
 }
