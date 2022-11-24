02296c028301531794e37d62dc9c30fd2ed7d7a7
KimJaeGeun <jgk9612@gmail.com>
2022-11-24 10:03:08 +0900

    feat: Add Codeup C1904 printOddNum with recursivefunction

---
+++ src/main/java/algorithm/codeup/C1904.java
@@ -0,0 +1,13 @@
+package algorithm.codeup;
+
+public class C1904 {
+
+    static void pritnOddNum(int x, int y) {
+
+        // 나눗셈 연산
+        if (x % 2 == 0) x++;
+        if (y % 2 == 0) y++;
+
+        
+    }
+}
