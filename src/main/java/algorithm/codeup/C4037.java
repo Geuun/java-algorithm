65a5e4842eb392ac4d8384e849a7b8bb93c166db
KimJaeGeun <jgk9612@gmail.com>
2022-12-02 10:11:05 +0900

    feat: Add findPrimeFactor algorithm with RecursionFunction

--- src/main/java/algorithm/codeup/C4037.java
+++ src/main/java/algorithm/codeup/C4037.java
@@ -16,11 +16,26 @@ public class C4037 {
         if (n != 1) System.out.printf("%d", n);
     }
 
+    static void findPrimeFactor(int n) {
+        for(int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                System.out.printf("%d", i);
+                findPrimeFactor(n / i);
+                break;
+            }
+        }
+        if (n != 1) System.out.printf("%d", n);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] input = bf.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
 
+        System.out.println("print primeFactorization(n):");
         primeFactorization(n);
+        System.out.println();
+        System.out.println("print findPrimeFactor(n):");
+        findPrimeFactor(n);
     }
 }
